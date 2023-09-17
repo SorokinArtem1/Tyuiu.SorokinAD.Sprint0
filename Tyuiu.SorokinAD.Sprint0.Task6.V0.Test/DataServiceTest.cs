a8db6a27c59e4f311b70bfe3b4fd771c511fcb37
Tyuiu.SorokinAD@outlook.com <abuzius@mail.ru>
2023-09-17 13:16:53 +0500

    Task6

---
+++ Tyuiu.SorokinAD.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SorokinAD.Sprint0.Task6.V0.Lib;
+
+
+namespace Tyuiu.SorokinAD.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+              
+        }
+        [TestMethod]
+        public void CheckSubstracktionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubstracktionArray(numbers);
+            Assert.AreEqual(-15, res);
+
+        }
+        [TestMethod]
+        public void CheckMultArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplucationArray(numbers);
+            Assert.AreEqual(120, res);
+
+        }
+    }
+}
---
+++ Tyuiu.SorokinAD.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SorokinAD.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.SorokinAD.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элементов масива = " + DataService.AdditionArray(numsArray));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubstracktionArray(numsArray));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplucationArray(numsArray));
+            Console.ReadKey();
+        }
+    }
+}
